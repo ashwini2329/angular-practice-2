1ae4ecaed69f0cb42c8d1dcdb7142dd0f5b342ce
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-09-28 20:35:56 +0530

    counter rendered using reducer. also counter function called from appcomponent

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
+  count$: Observable<number>
 
+  constructor(private store: Store<{counter: number}>) {
+    this.count$ = store.select('counter')
+  }
 }
