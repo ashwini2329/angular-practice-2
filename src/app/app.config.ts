083c0ff4437cc347e09d58a4e7fbb35d5bc8b687
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-09-28 12:16:58 +0530

    basic declaration of reducer function and using it in application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
+import {counterReducer } from './store/counter.reducer'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideStore({counter: counterReducer})]
 };
